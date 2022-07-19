cc86076196ed59c8b7c015605f1db4dfad91363f
SU_MIN_JING <goodcack@gmail.com>
2022-07-19 19:18:20 +0800

    Move TMDB API KEY to local.properties

--- app/src/main/java/com/jim/moviecritics/login/LoginDialog.kt
+++ app/src/main/java/com/jim/moviecritics/login/LoginDialog.kt
@@ -49,7 +49,7 @@ class LoginDialog : AppCompatDialogFragment() {
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.server_client_id))
+            .requestIdToken(getString(R.string.service_client_id))
             .requestEmail()
             .build()
 

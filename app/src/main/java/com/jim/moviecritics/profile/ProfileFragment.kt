839a15e2423a59ec5c275995b111628147e1015c
蘇民敬 <goodcack@gmail.com>
2025-08-20 10:36:08 +0800

    升級目標、編譯 SDK 版本至 35; 升級 gradle wrapper 版本; 重構會員資料頁的 Menu Host

--- app/src/main/java/com/jim/moviecritics/profile/ProfileFragment.kt
+++ app/src/main/java/com/jim/moviecritics/profile/ProfileFragment.kt
@@ -2,8 +2,11 @@ package com.jim.moviecritics.profile
 
 import android.os.Bundle
 import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import com.google.android.material.tabs.TabLayoutMediator
 import com.jim.moviecritics.R
 import com.jim.moviecritics.databinding.FragmentProfileBinding
@@ -16,29 +19,11 @@ class ProfileFragment : Fragment() {
         getVmFactory(ProfileFragmentArgs.fromBundle(requireArguments()).userKey)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
-        inflater.inflate(R.menu.toolbar_menu_log_out, menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.toolbar_log_out -> {
-                Logger.i("toolbar_button_log_out onClick")
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-
-        setHasOptionsMenu(true)
-
         FragmentProfileBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
             viewModel = profileViewModel
@@ -46,10 +31,31 @@ class ProfileFragment : Fragment() {
             viewpagerProfile.let {
                 it.adapter = ProfilePagerAdapter(childFragmentManager, lifecycle)
                 TabLayoutMediator(tabsProfile, it) { tab, position ->
-                    tab.text = ProfileTypeFilter.values()[position].value
+                    tab.text = ProfileTypeFilter.entries[position].value
                 }.attach()
             }
             return@onCreateView root
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.toolbar_menu_log_out, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.toolbar_log_out -> {
+                        Logger.i("toolbar_button_log_out onClick")
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
